26e756081df53cb728c1247db3d1efc4dae078a4
Malachi Hul <malachi.hul@gmail.com>
2025-12-26 19:11:44 +0900

    Initial commit.

---
+++ testTheVibe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  testTheVibe
+//
+//  Created by Malachi Hul on 2025/12/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ testTheVibe/testTheVibeApp.swift
@@ -0,0 +1,17 @@
+//
+//  testTheVibeApp.swift
+//  testTheVibe
+//
+//  Created by Malachi Hul on 2025/12/26.
+//
+
+import SwiftUI
+
+@main
+struct testTheVibeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
